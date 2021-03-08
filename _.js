a7e22ef1b9bcae15c9d9762bebbd244fabd0bf59
Ricardo Ramos <rf.ramos95@gmail.com>
2021-03-08 19:30:46 +0000

    Implementation of inRange, words and pad functions. Last two don't accept any specific pattern, working only with spaces

--- _.js
+++ _.js
@@ -3,6 +3,27 @@ const _ = {
         let lowerClampedValue = Math.max(number, lower);
         let clampedValue = Math.min(lowerClampedValue, upper);
         return clampedValue;
+    },
+    inRange(number, start, end) {
+        if (end == null) {
+            end = start;
+            start = 0;
+        }
+
+        return number >= start && number < end || number > end && number <= start;
+    },
+    words(string) {
+        return string.split(' ');
+    }, 
+    pad(string, length) {
+        if (string.length >= length)
+            return string;
+
+        let startPaddingLength = Math.floor((length - string.length) / 2);
+        let endPaddingLength = length - string.length - startPaddingLength;
+        let paddedString = ' ';
+        
+        return paddedString.repeat(startPaddingLength) + string + paddedString.repeat(endPaddingLength);
     }
 };
 
