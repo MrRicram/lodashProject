1402d1d422ae31d1b64becb56171a15920258f13
Ricardo Ramos <rf.ramos95@gmail.com>
2021-03-08 21:32:06 +0000

    Final commit! Implement of drop, dropWhile and chunk functions.

--- _.js
+++ _.js
@@ -45,6 +45,27 @@ const _ = {
         }
 
         return undefined;
+    },
+    drop(arr, nElements) {
+        if (nElements == null)
+            nElements = 1;
+
+        return arr.slice(nElements);
+    },
+    dropWhile(arr, predicate) {
+        let index = arr.findIndex((element, index) => !predicate(element, index, arr));
+        return this.drop(arr, index);
+    },
+    chunk(arr, size) {
+        if (size == null)
+            size = 1;
+
+        let newArr = [];
+        for (let i = 0; i < arr.length; i+=size){
+            newArr.push(arr.slice(i, i + size));
+        }
+
+        return newArr;
     }
 };
 
